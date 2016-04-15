a34eec82aab184dea9d98aad171b680639245b93
heikehuan <873089992@qq.com>
2016-04-16 02:20:00 +0800

    first commit

---
+++ src/main/webapp/js/silder.js
@@ -0,0 +1,23 @@
+/*������ǰҳ����*/
+var obj = null;
+var As = document.getElementById('topnav').getElementsByTagName('a');
+obj = As[0];
+for (i = 1; i < As.length; i++) {
+    if (window.location.href.indexOf(As[i].href) >= 0)
+        obj = As[i];
+}
+obj.id = 'topnav_current'
+
+/*�ٶȷ�����*/
+window._bd_share_config = {
+    "common": {
+        "bdSnsKey": {},
+        "bdText": "",
+        "bdMini": "1",
+        "bdMiniList": false,
+        "bdPic": "",
+        "bdStyle": "1",
+        "bdSize": "32"
+    }, "share": {}
+};
+with (document)0[(getElementsByTagName('head')[0] || body).appendChild(createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=' + ~(-new Date() / 36e5)];
