7501713968724eabac7e10fbbb052c7437bb20c5
huanhuan.zhan <huanhuan.zhan@ptthink.com>
2016-06-24 18:39:53 +0800

    master换成spring-boot

---
+++ src/main/java/com/huan/Application.java
@@ -0,0 +1,20 @@
+package com.huan;
+
+import com.huan.config.AbstractApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.UnknownHostException;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.huan")
+public class Application extends AbstractApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(Application.class);
+        abstractMain(app, args);
+    }
+}
